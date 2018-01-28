50ebc80ab946e9b20e8f1309deacfee0a815abc0
Johannes Katajamäki <jkatajamki@gmail.com>
2018-01-28 12:43:34 +0200

    added eslint to project & changed directory architecture

--- server/routing/app-routes.js
+++ server/routing/app-routes.js
@@ -1,7 +1,7 @@
 const HelloWorldHandler = require('./handlers/hello-world.handler');
 
 const routes = [
-  { path: '/helloworld', handler: HelloWorldHandler, },
+  { path: '/helloworld', handler: HelloWorldHandler },
 ];
 
 module.exports = { routes };
--- server/routing/routing.js
+++ server/routing/routing.js
@@ -2,9 +2,9 @@ const Four04Handler = require('./handlers/four04.handler');
 const appRoutes = require('./app-routes');
 
 const getHandler = (path) => {
-  const route = appRoutes.routes.find(route => route.path === path);
+  const foundRoute = appRoutes.routes.find(route => route.path === path);
 
-  return (route && route.handler) ? route.handler : Four04Handler;
+  return (foundRoute && foundRoute.handler) ? foundRoute.handler : Four04Handler;
 };
 
 module.exports = { getHandler };
