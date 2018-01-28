c55428d13fb5a8b8970bc604ffe27e5af45c6dc8
Johannes Katajamäki <jkatajamki@gmail.com>
2018-01-28 20:46:49 +0200

    added React client skeleton

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+const config = {
+  entry: path.resolve(__dirname, 'client/index.jsx'),
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'client/index.html'),
+    }),
+  ],
+};
+
+module.exports = config;
