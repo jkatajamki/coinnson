cbe8be45e89b15d41aecbcc6b5d1cab8c392ea9f
eevajonnapanula <eeva-jonna.panula@codento.com>
2019-04-01 18:58:59 +0300

    Reset points

--- coinsson-client/src/Admin/Admin.jsx
+++ coinsson-client/src/Admin/Admin.jsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AdminContainer, ResetContainer } from './styles';
 import ResetQuests from './ResetQuests';
 import TrackRowContainer from './TrackRowContainer';
 import ResetPoints from './ResetPoints';
+import apiCall from '../Ajax/ajax';
 
-const Admin = ({ tracks }) => {
-  const reset = () => {
-    
-  }
+const Admin = ({ tracks, refetchPoints }) => {
+  const resetPoints = () => {
+    const path = '/content/resetPoints';
+    const method = 'GET';
+    return apiCall(method, path);
+  };
+  
+  const resetQuests = () => {};
+  
   return (
     <AdminContainer>
       <h2>Reset</h2>
       <ResetContainer>
-        <ResetQuests resetQuests={reset} />
-        <ResetPoints resetPoints={reset} />
+        <ResetQuests resetQuests={resetQuests} />
+        <ResetPoints resetPoints={resetPoints} />
       </ResetContainer>
       <TrackRowContainer tracks={tracks} />
     </AdminContainer>
