dfdc6b481991eceecdc2e7a2a6c82913655440c8
Johannes Katajamäki <jkatajamki@gmail.com>
2019-04-04 20:11:01 +0300

    Allow CORS for all envs

--- server/server.js
+++ server/server.js
@@ -8,22 +8,12 @@ import { getAllQuests, getAllTracks } from './contentful/contentful';
 dotenv.config();
 
 const port = process.env.PORT || 5000;
-const { ENV, CLIENT_ORIGIN } = process.env;
-
-function isCorsEnabled() {
-  if (ENV.includes('dev') || CLIENT_ORIGIN.includes('coinsson')) {
-    return true;
-  }
-
-  return false;
-}
+const { CLIENT_ORIGIN } = process.env;
 
 function mangleCors(res) {
-  if (isCorsEnabled()) {
-    res.setHeader('Access-Control-Allow-Origin', CLIENT_ORIGIN);
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-  }
+  res.setHeader('Access-Control-Allow-Origin', CLIENT_ORIGIN);
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   return res;
 }
 
