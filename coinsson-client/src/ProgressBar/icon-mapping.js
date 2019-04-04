8d4df1bb6e03572468bd43056876acb78da6439f
Johannes Katajamäki <jkatajamki@gmail.com>
2019-04-05 00:15:48 +0300

    Icon mappings for actual quests

--- coinsson-client/src/ProgressBar/icon-mapping.js
+++ coinsson-client/src/ProgressBar/icon-mapping.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { FaFly, FaPlaneDeparture, FaCheck, FaCcVisa } from 'react-icons/fa';
+import { FaFly, FaBomb, FaCarCrash, FaBusinessTime, FaBeer } from 'react-icons/fa';
 
 export const icons = {
-  Laskuvarjohyppy: <FaFly />,
-  Matka: <FaPlaneDeparture />,
-  Done: <FaCheck />,
-  Tutorial: <FaCcVisa />
+  Tutorial: <FaFly />,
+  'Master Of War': <FaBomb />,
+  'End Of The Road': <FaCarCrash />,
+  'The Architecture Of Marriage': <FaBusinessTime />,
+  'Beer O\'Clock': <FaBeer />
 };
