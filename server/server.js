925924ba599d847067d037c7418ae698e3caba8f
Johannes Katajamäki <jkatajamki@gmail.com>
2019-04-04 20:22:19 +0300

    Allow CORS for all envs

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ import { getAllQuests, getAllTracks } from './contentful/contentful';
 dotenv.config();
 
 const port = process.env.PORT || 5000;
-const { CLIENT_ORIGIN_DEV } = process.env;
+const { CLIENT_ORIGIN, CLIENT_ORIGIN_DEV } = process.env;
 
 function mangleCors(res) {
   res.setHeader('Access-Control-Allow-Origin', CLIENT_ORIGIN);
